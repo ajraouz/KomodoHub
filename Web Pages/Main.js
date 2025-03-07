64030b158de99782601be942a00c06916c5e9932
ajraouz <ajraouz@coventry.ac.uk>
2025-03-07 17:00:03 +0000

    Profile goes on school

--- Web Pages/Main.js
+++ Web Pages/Main.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginButton = document.querySelector(".login");
-    const storedUser = localStorage.getItem("username");
+    const storedUser = localStorage.getItem("userType");
 
-    if (storedUser) {
+    if (storedUser === 'principal') {
         loginButton.textContent = "Profile";
         loginButton.addEventListener("click", function () {
-            window.location.href = "/Web Pages/ProfilePrivateView.html"; // Redirect to login
+            window.location.href = "/Web Pages/SchoolPrivatePage.html";
         });
-    } else {
+    }
+    else if (storedUser === 'student' || storedUser === 'teacher' || storedUser === 'member' || storedUser === 'admin'){
+        loginButton.textContent = "Profile";
+        loginButton.addEventListener("click", function () {
+            window.location.href = "/Web Pages/ProfilePrivateView.html";
+        });
+    }
+    else {
         loginButton.textContent = "Login";
         loginButton.addEventListener("click", function () {
             window.location.href = "/Web Pages/LoginPage.html"; // Redirect to login
